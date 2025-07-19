c89a8db9ae6729f18f56c35259af41e9ccc31c66
helloyongyang <yongyang1030@163.com>
2025-07-19 10:57:09 +0800

    5

--- run_qwenvl2.sh
+++ run_qwenvl2.sh
@@ -1,6 +1,6 @@
 pip install transformers==4.53.2
 
 python run_qwenvl.py \
---model-path /data/nvme1/yongyang/projects/llmc_plus/Qwen2.5-VL-3B-Instruct \
+--model-path /data/nvme1/yongyang/projects/llmc_plus/Qwen2.5-VL-7B-Instruct \
 --question-file turn2_question.jsonl \
 --max_new_tokens 1024
